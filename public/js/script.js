c33d355e6d9d5b2f5d0b555bb62dd397f5b16a82
ROEL2407 <roel24komen@gmail.com>
2022-04-22 10:35:09 +0200

    Update: make function to make guess word

--- public/js/script.js
+++ public/js/script.js
@@ -1,16 +1,35 @@
-let socket = io()
-let messages = document.querySelector('section ul')
-let input = document.querySelector('input')
+let socket = io();
+let messages = document.querySelector('section ul');
+const input = document.querySelector('#chat input');
 
 document.querySelector('form').addEventListener('submit', event => {
-  event.preventDefault()
+  event.preventDefault();
   if (input.value) {
-    socket.emit('message', input.value)
-    input.value = ''
+    socket.emit('message', input.value);
+    input.value = '';
   }
 })
 
 socket.on('message', message => {
-  messages.appendChild(Object.assign(document.createElement('li'), { textContent: message }))
-  messages.scrollTop = messages.scrollHeight
+  messages.appendChild(Object.assign(document.createElement('li'), { textContent: message }));
+  messages.scrollTop = messages.scrollHeight;
+})
+
+const wordInput = document.querySelector('#wordInput');
+let wordHidden = [];
+
+
+const btn = document.querySelector("#makeWord button");
+btn.addEventListener("click", function() {
+  let word = wordInput.value;
+  
+  for (var i = 0; i < word.length; i++) {
+    wordHidden[i] = "_";
+  }
+  console.log(wordHidden);
+  wordHidden.forEach(item => {
+    tempItem = document.createElement('span');
+    tempItem.innerHTML = item;
+    document.getElementById('presentWord').appendChild(tempItem);
+  })
 })
