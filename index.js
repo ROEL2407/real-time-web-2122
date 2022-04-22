f873786532e023dc8f58e66466cef1ab4121c86a
ROEL2407 <roel24komen@gmail.com>
2022-04-22 14:05:06 +0200

    Update: implemented urban dictionary api

--- index.js
+++ index.js
@@ -3,7 +3,19 @@ const app = express();
 const http = require("http").createServer(app);
 const path = require("path");
 const io = require("socket.io")(http);
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
 const port = process.env.PORT || 4200;
+
+const url = 'https://mashape-community-urban-dictionary.p.rapidapi.com/define?term=wat';
+
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'mashape-community-urban-dictionary.p.rapidapi.com',
+    'X-RapidAPI-Key': 'a7685906d1mshf07861808c518c4p1d4ee6jsn758616f5bc0f'
+  }
+};
 
 app.set("view engine", "ejs");
 
@@ -24,6 +36,11 @@ io.on("connection", (socket) => {
     console.log("user disconnected");
   });
 });
+
+fetch(url, options)
+	.then(res => res.json())
+	.then(json => console.log(json))
+	.catch(err => console.error('error:' + err));
 
 http.listen(port, () => {
   console.log("listening on port ", port);
