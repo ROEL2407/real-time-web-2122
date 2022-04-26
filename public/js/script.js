792dd485b29d510b5c30d4684dd293155b6d1ccc
ROEL2407 <roel24komen@gmail.com>
2022-04-26 13:51:06 +0200

    Update: check if you guessed the word correctly

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,11 @@
+
 let socket = io();
 socket.emit('create', 'room1');
 
 let messages = document.querySelector('section ul');
 const input = document.querySelector('#chat input');
 
+// sends messages in the chat
 document.querySelector('#chat form').addEventListener('submit', event => {
   event.preventDefault();
   if (input.value) {
@@ -48,12 +50,14 @@ socket.on('message', message => {
 
 
 let wordStatus = null;
+let guessWord = '';
+let wordDef = '';
 let guessed = [];
 
 function generateButtons() {
   let buttonsHTML = 'abcdefghijklmnopqrstuvwxyz'.split('').map(letter =>
     `
-    <button class="" id '` + letter +`' onClick="handleGuess('` + letter + `')">
+    <button class="btn" id='` + letter +`' onClick="handleGuess(\'` + letter + `\')"">
     ` + letter + `
     </button>
     `
@@ -63,12 +67,42 @@ function generateButtons() {
 }
 
 socket.on('newWord', newWord => {
-  
-    //if letter exsists in word then letter has bigger value than 0. If it is letter is _
-    wordStatus = newWord.word.split('').map(letter => (guessed.indexOf(letter) >= 0 ? letter : " _ ")).join("");
+      guessed = [];
+      let keyboard_btn = document.querySelectorAll('.btn');
+      keyboard_btn.forEach( item => {
+        item.removeAttribute('disabled');
+      })
+      //if letter exsists in word then letter has bigger value than 0. If it is letter is _
+      guessWord = newWord.word.toLowerCase();
+      wordDef = newWord.definition;
+      console.log(newWord.word)
+    wordStatus = guessWord.split('').map(letter => (guessed.indexOf(letter) >= 0 ? letter : " _ ")).join("");
   
     document.getElementById('presentWord').innerHTML = wordStatus;
+
 })
 
+function handleGuess(chosenLetter) {
+  guessed.indexOf(chosenLetter) === -1 ? guessed.push(chosenLetter) : null;
+  document.getElementById(chosenLetter).setAttribute('disabled', true)
+
+  if (guessWord.indexOf(chosenLetter) >= 0) {
+    wordStatus = guessWord.split('').map(letter => (guessed.indexOf(letter) >= 0 ? letter : " _ ")).join("");
+  
+    document.getElementById('presentWord').innerHTML = wordStatus;
+    console.log(wordStatus);
+    if (!wordStatus.includes('_')) {
+      winner();
+    }
+  }
+}
+
+function winner() {
+  const tempItem = document.createElement('div');
+  tempItem.setAttribute("class", "winner pop");
+  const output = '<h3>Winner!</h3>';
+  tempItem.innerHTML = output;
+  document.querySelector('main').appendChild(tempItem);
+}
+
 generateButtons();
-// guessedWord();
